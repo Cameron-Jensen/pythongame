eff975b31cdee91da1c0c7cc46e8c9171f34a813
Cameron-Jensen <twobecomeonetbo@gmail.com>
2025-03-10 14:17:57 -0500

    collisions + other things

--- main.py
+++ main.py
@@ -30,6 +30,10 @@ def main():
       for event in pygame.event.get():
           if event.type == pygame.QUIT:
               return
+      for sprite in asteroid:
+            if sprite.collision(player) == True:
+                print("Game over!")
+                exit() 
       dt2=clock.tick(60)
       updateable.update(dt)
       dt=dt2/1000
